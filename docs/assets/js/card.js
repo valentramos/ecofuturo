8c34627ae2e8322365cf6e8064d8dd0323b05b22
valentramos <ignacio1994.ir@gmail.com>
2020-12-08 12:38:30 -0400

    release 1.2

--- docs/assets/js/card.js
+++ docs/assets/js/card.js
@@ -1,103 +1,126 @@
-
-var cartas = new Array( 
-    {nombre: '1', seleccion: false}, {nombre: '2', seleccion: false}, 
-    {nombre: '3', seleccion: false}, {nombre: '4', seleccion: false}, 
-    {nombre: '5', seleccion: false}, {nombre: '6', seleccion: false}, 
-    {nombre: '7', seleccion: false}, {nombre: '8', seleccion: false}, 
-    {nombre: '1', seleccion: false}, {nombre: '2', seleccion: false}, 
-    {nombre: '3', seleccion: false}, {nombre: '4', seleccion: false}, 
-    {nombre: '5', seleccion: false}, {nombre: '6', seleccion: false}, 
-    {nombre: '7', seleccion: false}, {nombre: '8', seleccion: false}
-);
-          
-  var intentos = 0;
-  var jugada1 = "";
-  var jugada2 = "";
-  var identificadorJ1 = "";
-  var identificadorJ2 = "";
-  
-  function iniciarJuego () {	
-    var dato = document.getElementById("juego");
-    dato.style.opacity = 1;
-  
-    cartas.sort(function() {return Math.random() - 0.5});
-    for ( var i = 0 ; i < 16 ; i++ ) {
-      var carta = cartas[i].nombre;
-      var dato = document.getElementById( i.toString() );
-      dato.dataset.valor = carta;
+const initGameCards = () => {
+  $(document).ready(function(){
+    
+    var checkArray = []; // para verificar si las dos cartas con click son el mismo personaje
+    var idArray = []; //array para guardar los ids de las cartas que tienen click 
+    var contador = 0;
+    var fin = 0; 
+    var fields = document.querySelectorAll(".atras");
+    
+    
+    var images = [
+    "assets/images/module-3/card-game/1.png",
+    "assets/images/module-3/card-game/2.png",
+    "assets/images/module-3/card-game/3.png",
+    "assets/images/module-3/card-game/4.png",
+    "assets/images/module-3/card-game/5.png",
+    "assets/images/module-3/card-game/6.png",
+    "assets/images/module-3/card-game/7.png",
+    "assets/images/module-3/card-game/8.png",
+    "assets/images/module-3/card-game/9.png",
+    "assets/images/module-3/card-game/1.png",
+    "assets/images/module-3/card-game/2.png",
+    "assets/images/module-3/card-game/3.png",
+    "assets/images/module-3/card-game/4.png",
+    "assets/images/module-3/card-game/5.png",
+    "assets/images/module-3/card-game/6.png",
+    "assets/images/module-3/card-game/7.png",
+    "assets/images/module-3/card-game/8.png",
+    "assets/images/module-3/card-game/9.png"
+    ];
+    // verificacion de los clicks
+    function clicked() { 
+      if ($(this).find(".inner-wrap").hasClass("flipped")) {
+        return;
+      }
+      $(this).find(".inner-wrap").toggleClass("flipped");
+      checkArray.push($(this).find("img").attr("src"));
+      idArray.push($(this).attr("id"));
+      check();
     }
-  };
-  
-  function resetearJuego () {
-    cartas.sort(function() {return Math.random() - 0.5});
-    for ( var i = 0 ; i < 16 ; i++ ) {
-      var carta = cartas[i].nombre;
-      var dato = document.getElementById( i.toString() );
-      dato.dataset.valor = carta;
-      colorCambio( i, 'black', '?');
-    }	
-  }
-  
-  function girarCarta () {
-    var evento = window.event;
-  
-    jugada2 = evento.target.dataset.valor;
-    identificadorJ2 = evento.target.id;
-  
-  
-    if ( jugada1 !== "" ) {
-  
-      if ( jugada1 === jugada2 && identificadorJ1 !== identificadorJ2 && cartas[parseInt(identificadorJ2)].seleccion != true &&               cartas[parseInt(identificadorJ1)].seleccion != true) {
-        
-        cartas[parseInt(identificadorJ1)].seleccion = true;
-        cartas[parseInt(identificadorJ2)].seleccion = true;
-  
-        colorCambio(identificadorJ2, "blue", jugada2);
-        vaciar();
-        comprobar();
-      }else if(identificadorJ1 !== identificadorJ2){
-        var self = this;
+    
+    $(".carta").on("click", clicked);
+      
+    //reiniciar el juego
+    function reiniciar() {
+      $(".atras").find("img").remove(); //quitar todas las imagenes actuales
+      $(".carta .inner-wrap").removeClass("flipped"); // quitar la classe flipped para volver a su estado inicial
+      checkArray = []; 
+      idArray = [];
+      contador = 0; 
+      fin = 0;
+      iniciarJuego();
+    }
+    //para verificar el fin del juego
+    function verificarFin() {
+      if (fin === 18) { //si todas las cartas estan volteadas
+        alert("Juego finalizado, lo has logrado en " + contador + " intentos");
+        reiniciar();
+      }
+    }
+    //para random de las imagenes 
+    function shuffleArray(array) { 
+        for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        return array;
+    }
+    
+    function iniciarJuego() {
+    
+      
+    
+      var arr = shuffleArray(images); //array con las imagenes de forma aleatoria
+     // append de las imagenes a la clase para la parte de atras de las cartas
+      for (var i = 0; i < fields.length; i++) {
+        var img = document.createElement("img");
+        img.src = arr[i];
+        fields[i].appendChild(img);
+      }
+    
+    
+    }
+    
+    function check() {
+      //si los fields se  han hecho dos clicks 
+      if (checkArray.length === 2) {
+        $(".carta").off("click", clicked); 
         setTimeout(function(){
-          colorCambio(self.identificadorJ1, "black", "?")
-          colorCambio(self.identificadorJ2, "black", "?")
-          vaciar()
-        },1000); 
-  
-        colorCambio(identificadorJ2, "blue", jugada2);
+          //si no hay match
+          if (checkArray[0] !== checkArray[1]) { 
+            //voltear las dos cartas seleccionadas
+            $("#" + idArray[0]).find(".inner-wrap").removeClass("flipped"); 
+            $("#" + idArray[1]).find(".inner-wrap").removeClass("flipped"); 
+            contador++;
+            //vaciar los arrays para la siguiente eleccion
+            checkArray = []; 
+            idArray = []; 
+            //habilitar el click de nuevo
+            $(".carta").on("click", clicked);
+          } else {
+    
+            contador++;
+            
+            fin += 2; // contador para el final del juego, se agregan dos para el contador de fin
+            //vaciar los dos arrays
+            checkArray = []; 
+            idArray = []; 
+            verificarFin(); 
+            $(".carta").on("click", clicked); 
+          }
+          document.querySelector(".counter").innerHTML = contador;
+        }, 800);	
       }
-    } else if(jugada2 !== "valor") {
-  
-      colorCambio(identificadorJ2, "blue", jugada2);
-  
-      jugada1 = jugada2;
-      identificadorJ1 = identificadorJ2;
     }
-  };
-  
-  function vaciar ()  {
-    jugada1 = "";	
-    jugada2 = "";	
-  
-    identificadorJ1 = "";
-    identificadorJ2 = "";
-  }
-  
-  function colorCambio (posicion, color, contenido) {
-    document.getElementById(posicion.toString()).style.backgroundColor = color;
-    document.getElementById(posicion.toString()).innerHTML = contenido;
-  }	
-  
-  function comprobar () {
-    var aciertos = 0;
-    for( var i = 0 ; i < 16 ; i++ ){
-      if ( cartas[i].seleccion == true ) {
-        aciertos ++;
-      }
-  
-    }
-  
-    if(aciertos == 16){
-      document.getElementById("juego").innerHTML = "GANASTE";
-    }
-  }
-  
+    
+    
+    
+    iniciarJuego();
+    
+    });
+}
+
+// initGameCards();
