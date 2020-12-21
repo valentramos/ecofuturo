345f56a9ba1cbd8f9ca7e971af4d889e2908c9a7
valentramos <ignacio1994.ir@gmail.com>
2020-12-21 17:12:36 -0400

    release 1.1.2

--- docs/assets/js/video-controls.js
+++ docs/assets/js/video-controls.js
@@ -8,3 +8,29 @@ const playVideo = () => {
 	$('#sample_video').trigger('play'); 
 }
 // videoControls();
+
+const dataPicker = () => {
+	var dateClass='.datechk';
+	$(document).ready(function ()
+	{
+	  if (document.querySelector(dateClass).type !== 'date')
+	  {
+		var oCSS = document.createElement('link');
+		oCSS.type='text/css'; oCSS.rel='stylesheet';
+		oCSS.href='//ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/base/jquery-ui.css';
+		oCSS.onload=function()
+		{
+		  var oJS = document.createElement('script');
+		  oJS.type='text/javascript';
+		  oJS.src='//ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js';
+		  oJS.onload=function()
+		  {
+			$(dateClass).datepicker();
+		  }
+		  document.body.appendChild(oJS);
+		}
+		document.body.appendChild(oCSS);
+	  }
+	});
+}
+// dataPicker();
