fcc67513871efd250b1b0ca03c8dfb2e80f0e4f5
valentramos <ignacio1994.ir@gmail.com>
2021-03-05 14:32:59 -0400

    adds

--- docs/assets/js/audio-controls.js
+++ docs/assets/js/audio-controls.js
@@ -12,7 +12,7 @@ const stopAudio = () => {
 		}
 		}
 	});
-  /*********** /Utility ***********/  
+  	/*********** /Utility ***********/  
   
 	// $('#stop-audio-btn').click(function() {
 	// 	$('audio').stopAudio();
